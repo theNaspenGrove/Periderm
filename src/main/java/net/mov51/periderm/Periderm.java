6dcde7c2c2f2e68dfc853ba362ade87240283ace
mov <mov51@mov51.net>
2022-09-25 20:04:02 -0400

    Make Periderm a standalone API jar
    May the Periderm protect us all

---
+++ src/main/java/net/mov51/periderm/Periderm.java
@@ -0,0 +1,11 @@
+package net.mov51.periderm;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Periderm extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        this.getLogger().log(java.util.logging.Level.INFO, "May the Periderm protect you!");
+    }
+}
+
